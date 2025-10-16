2977e5628632890d360210ada8d3f4b5a766281e
K0iiish <marilagosa@udd.cl>
2025-10-16 19:23:15 -0300

    nuevos avances 16 de octubre

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:flutter_application_1/screens/splashscreen.dart';
 import 'package:flutter_application_1/screens/chats.dart';
+import 'package:flutter_application_1/screens/login.dart';
 import 'package:flutter_application_1/theme/theme.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  try {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDrGeP6Y2Lk2YByyacMX5GS6kLbxAtFvYg",
+        authDomain: "app1-a0273.firebaseapp.com",
+        projectId: "app1-a0273",
+        storageBucket: "app1-a0273.firebasestorage.app",
+        messagingSenderId: "122655108877",
+        appId: "1:122655108877:web:f27788f9c0b9c35a3c8a44",
+      ),
+    );
+  } on FirebaseException catch (e) {
+    // Si la app ya estaba inicializada, ignora el error
+    if (e.code != 'duplicate-app') rethrow;
+  }
+
   runApp(const MainApp());
 }
 
@@ -17,6 +36,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
@@ -28,8 +48,29 @@ class MainApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void _signOut(BuildContext context) async {
+    await GoogleSignIn.instance.signOut();
+    await FirebaseAuth.instance.signOut();
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const ChatsScreen();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inicio'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => _signOut(context),
+          ),
+        ],
+      ),
+      body: const ChatsScreen(),
+    );
   }
 }
